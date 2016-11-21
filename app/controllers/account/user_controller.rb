79ebb43c897b2a02ca03f9036fbc1952a74f1e07
YoannGueguen <yoann-gueguen@hotmail.fr>
2016-11-21 20:42:49 +0100

    return on master

--- app/controllers/account/user_controller.rb
+++ app/controllers/account/user_controller.rb
@@ -10,6 +10,6 @@ class Account::UserController < Account::AccountController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
