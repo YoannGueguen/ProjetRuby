71beb0ffd0314392b890b640f1e23a60a6f0e5e4
YoannGueguen <yoann-gueguen@hotmail.fr>
2016-11-22 11:51:55 +0100

    finish footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def locale_select (lang)
     out = "<select class='form-control pull-right language' data-do='setLocale'>"
     I18n.available_locales.each do |loc|
-      out += "<option value='en'#{url_for(locale: loc.to_s)}' #{'selected'if I18n.locale == loc }>#{loc}</option>"
+      out += "<option value=''#{url_for(locale: loc.to_s)}' #{'selected'if I18n.locale == loc }>#{loc}</option>"
     end
     out += "</select>"
     out.html_safe
